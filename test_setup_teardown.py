305deeccc80c09a91932f8421419efb4f58332e2
mengdegong <mengdegong@blued.com>
2020-12-09 21:39:41 +0800

    第一次作业

---
+++ test_setup_teardown.py
@@ -0,0 +1,44 @@
+import pytest
+
+
+def setup_module():
+    print('setup：整个test_setup_teardown模块开始只执行一次')
+
+def teardown_module():
+    print('teardown：整个test_setup_teardown模块结束只执行一次')
+
+def setup_function():
+    print("setup：不在类中的测试用例开始的时候都会执行")
+
+
+def teardown_function():
+    print("teardown：不在类中的测试用例结束的时候都会执行")
+
+
+def test_tree():
+    print("test-three")
+
+
+def test_four():
+    print("test-four")
+
+
+class TestClass():
+
+    def setup_class(self):
+        print("setup：所有用例开始执行一次")
+
+    def setup_method(self):
+        print("setup：每个用例开始执行")
+
+    def teardown_method(self):
+        print("teardown：每个用例结束执行")
+
+    def teardown_class(self):
+        print("teardown：所有用例结束执行一次")
+
+    def test_one(self):
+        print("test-one")
+
+    def test_two(self):
+        print("test-two")
